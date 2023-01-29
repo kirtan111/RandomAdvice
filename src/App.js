25fd2b7c11c5984283d612945c1d6cbae32c834f
kirtan111 <kirtanthummar7103@protonmail.com>
2023-01-29 15:48:00 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [advice, setAdvice] = useState("");
+
+    useEffect(() => {
+        fetchAdvice();
+    }, []);
+
+    const fetchAdvice = async () => {
+        try {
+            const url = await fetch(`https://api.adviceslip.com/advice`);
+            const data = await url.json();
+            setAdvice(data.slip.advice);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div className="app">
+            <div className="card">
+                <h1 className="heading"> {advice} </h1>
+                <button className="button" onClick={fetchAdvice}>
+                    <span>Give an Advice !</span>
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
